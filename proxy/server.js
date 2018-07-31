8e6b043e5f3c4a70df7827f60c74047bff3f3e1f
John Lukenoff <jlukenoff@gmail.com>
2018-07-30 19:00:35 -0700

    initial proxy set-up

---
+++ proxy/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => { console.log(`server running on port: ${port}`); });
+
+
